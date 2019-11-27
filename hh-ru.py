891ef77c86edff3d78bf24264b740bb1edc9d5ee
JsvnSvn <codejseven@gmail.com>
2019-11-27 22:10:45 +0300

    fix search list

--- hh-ru.py
+++ hh-ru.py
@@ -1,22 +1,20 @@
 import requests
 from bs4 import BeautifulSoup as bs
 import csv
+from random import choice
 
 
-
-base_url = 'https://hh.ru/search/vacancy?L_is_autosearch=false&clusters=true&enable_snippets=true&text=QA&page=0'
-
-
-def get_hh(base_url):
+def get_hh(base_url, headers=None, proxy=None):
     session = requests.Session()
-    r = session.get(base_url, headers={'accept': '*/*',
-                                       'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) '
-                                                     'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 '
-                                                     'Safari/537.36'})
+    r = session.get(base_url, headers=headers, proxies=proxy)
+    print(headers)
+    print(proxy)
     if r.status_code == 200:
         jobs = []
         soup = bs(r.content, 'html.parser')
-        divs = soup.find_all('div', attrs={'data-qa': 'vacancy-serp__vacancy'})
+        #h2 = soup.find_all('h2', class_='position')
+        divs = soup.find_all('div', class_='vacancy-serp-item')
+        #divs = soup.find_all('div', attrs={'data-qa': 'vacancy-serp__vacancy'})
         for div in divs:
             try:
                 title = div.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-title'}).text
@@ -60,7 +58,7 @@ def get_hh(base_url):
                          'description': description,
                          'requirements': requirements,
                          'url': url})
-        print(len(divs))
+        print(jobs)
     else:
         print('ERROR')
 
@@ -83,7 +81,14 @@ def write_csv(data):
 
 
 def main():
-    get_hh(base_url)
+    #base_url = 'https://ru.jooble.org/%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D0%B0/%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0?salary=20000'
+    base_url = 'https://hh.ru/search/vacancy?L_is_autosearch=false&clusters=true&enable_snippets=true&text=QA&page=0'
+    useragents = open('useragents.txt').read().split('\n')
+    proxies = open('proxies.txt').read().split('\n')
+    proxy = {'http': 'http://' + choice(proxies)}
+    headers = {'User-Agent': str(choice(useragents))}
+    get_hh(base_url, headers, proxy)
+
 
 
 if __name__ == '__main__':
