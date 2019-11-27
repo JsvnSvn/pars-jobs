96d407abaca221c22490dd61b7b231ce7c9b002f
JsvnSvn <codejseven@gmail.com>
2019-11-27 23:41:49 +0300

    added a record to csv

--- hh-ru.py
+++ hh-ru.py
@@ -4,91 +4,101 @@ import csv
 from random import choice
 
 
+def write_csv(jobs):
+    with open('hh.csv', 'a', newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(('Название вакансии', 'Зарплата', 'Компания', 'Город', 'Описание', 'Требования', 'Ссылка'))
+        for job in jobs:
+            writer.writerow((job['title'],
+                             job['salary'],
+                             job['company'],
+                             job['city'],
+                             job['description'],
+                             job['requirements'],
+                             job['url']))
+
+
 def get_hh(base_url, headers=None, proxy=None):
+    jobs = []
+    urls = []
+    urls.append(base_url)
     session = requests.Session()
     r = session.get(base_url, headers=headers, proxies=proxy)
-    print(headers)
-    print(proxy)
     if r.status_code == 200:
-        jobs = []
+        #r = session.get(base_url, headers=headers, proxies=proxy)
         soup = bs(r.content, 'html.parser')
-        #h2 = soup.find_all('h2', class_='position')
-        divs = soup.find_all('div', class_='vacancy-serp-item')
-        #divs = soup.find_all('div', attrs={'data-qa': 'vacancy-serp__vacancy'})
-        for div in divs:
-            try:
-                title = div.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-title'}).text
-            except:
-                title = ''
+        divs = soup.find_all('div', attrs={'class': 'vacancy-serp-item'})
+        try:
+            pagination = soup.find_all('a', attrs={'data-qa': 'pager-page'})
+            count = int(pagination[-1].text)
+            for i in range(count):
+                url = f'https://hh.ru/search/vacancy?L_is_autosearch=false&clusters=true&enable_snippets=true&text=QA&page={i}'
+                if url not in urls:
+                    urls.append(url)
+        except:
+            pass
 
-            try:
-                company = div.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-employer'}).text
-            except:
-                company = ''
+        for url in urls:
+            #r = session.get(base_url, headers=headers, proxies=proxy)
+            #soup = bs(r.content, 'html.parser')
+            #divs = soup.find_all('div', attrs={'class': 'vacancy-serp-item'})
+            for div in divs:
+                try:
+                    title = div.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-title'}).text
+                except:
+                    title = ''
 
-            try:
-                city = div.find('span', attrs={'data-qa': 'vacancy-serp__vacancy-address'}).text
-            except:
-                city = ''
+                try:
+                    company = div.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-employer'}).text
+                except:
+                    company = ''
 
-            try:
-                salary = div.find('div', attrs={'data-qa': 'vacancy-serp__vacancy-compensation'}).text
-            except:
-                salary = ''
+                try:
+                    city = div.find('span', attrs={'data-qa': 'vacancy-serp__vacancy-address'}).text
+                except:
+                    city = ''
 
-            try:
-                url = div.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-title'})['href']
-            except:
-                url = ''
+                try:
+                    salary = div.find('div', attrs={'data-qa': 'vacancy-serp__vacancy-compensation'}).text
+                except:
+                    salary = ''
 
-            try:
-                description = div.find('div', attrs={'data-qa': 'vacancy-serp__vacancy_snippet_responsibility'}).text
-            except:
-                description = ''
+                try:
+                    url = div.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-title'})['href']
+                except:
+                    url = ''
 
-            try:
-                requirements = div.find('div', attrs={'data-qa': 'vacancy-serp__vacancy_snippet_requirement'}).text
-            except:
-                requirements = ''
+                try:
+                    description = div.find('div', attrs={'data-qa': 'vacancy-serp__vacancy_snippet_responsibility'}).text
+                except:
+                    description = ''
 
-            jobs.append({'title': title,
-                         'salary': salary,
-                         'company': company,
-                         'city': city,
-                         'description': description,
-                         'requirements': requirements,
-                         'url': url})
-        print(jobs)
+                try:
+                    requirements = div.find('div', attrs={'data-qa': 'vacancy-serp__vacancy_snippet_requirement'}).text
+                except:
+                    requirements = ''
+
+                jobs.append({'title': title,
+                             'salary': salary,
+                             'company': company,
+                             'city': city,
+                             'description': description,
+                             'requirements': requirements,
+                             'url': url})
+            print(len(jobs))
     else:
         print('ERROR')
-
-
-
-
-
-
-
-def write_csv(data):
-    with open('hh.csv', 'a') as f:
-        writer = csv.writer(f)
-        writer.writerow((data['title'],
-                         data['zarplata'],
-                         data['vacancy_date'],
-                         data['url']))
-
-
-
+    return jobs
 
 
 def main():
-    #base_url = 'https://ru.jooble.org/%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D0%B0/%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0?salary=20000'
     base_url = 'https://hh.ru/search/vacancy?L_is_autosearch=false&clusters=true&enable_snippets=true&text=QA&page=0'
     useragents = open('useragents.txt').read().split('\n')
     proxies = open('proxies.txt').read().split('\n')
     proxy = {'http': 'http://' + choice(proxies)}
     headers = {'User-Agent': str(choice(useragents))}
-    get_hh(base_url, headers, proxy)
-
+    jobs = get_hh(base_url, headers, proxy)
+    write_csv(jobs)
 
 
 if __name__ == '__main__':
